99c11e89c0c7fa8d99fe263e5ab7a1f34d4ee2e5
jadenchoi94 <jchoi138@student.solbridge.ac.kr>
2021-06-21 11:52:37 +0900

    add templates

--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-
-
-
 # Create your views here.
+from django.shortcuts import render
+
+
 def hello_world(request):
-    return HttpResponse('Hello world!')
+    return render(request, 'base.html')
