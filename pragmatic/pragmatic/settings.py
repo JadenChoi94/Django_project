ee16b7fc28d6967bcc39bb875c483b6aae409308
jadenchoi94 <jchoi138@student.solbridge.ac.kr>
2021-06-21 22:13:30 +0900

    media,pillow설치

--- pragmatic/pragmatic/settings.py
+++ pragmatic/pragmatic/settings.py
@@ -146,3 +146,6 @@ LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
